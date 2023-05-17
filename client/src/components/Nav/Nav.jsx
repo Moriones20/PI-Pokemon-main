30bbac1066155fb558558eb6e706711dc1f3105f
Felipe Moriones <moriones06082001@gmail.com>
2023-05-16 19:54:15 -0500

    Create CSS to navbar and change default scroll

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,16 +1,34 @@
 import "./Nav.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import iconPokemon from "../../Assets/pokemon-icon.png";
+import iconLogout from "../../Assets/logout.png";
 
 const Nav = () => {
+  const location = useLocation();
+
   return (
     <nav>
       <div className="nav-container">
-        <NavLink to={"/home"}>
-          <button>Home</button>
-        </NavLink>
-        <NavLink to={"/create"}>
-          <button>Create Pokemon</button>
-        </NavLink>
+        <div className="icon-pokemon">
+          <img src={iconPokemon} alt="Pokemon-icon" />
+        </div>
+        <div className="links">
+          {location.pathname !== "/home" && (
+            <NavLink to={"/home"}>
+              <button className="btn-home">Home</button>
+            </NavLink>
+          )}
+          {location.pathname !== "/create" && (
+            <NavLink to={"/create"}>
+              <button className="btn-create-pokemon">Create Pokemon</button>
+            </NavLink>
+          )}
+        </div>
+        <div className="logout">
+          <NavLink to={"/"}>
+            <img src={iconLogout} alt="logout" />
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
